fa4c4734d94438befee14325d96c76ce51cfd2f9
vjszombathy <45408709+vjszombathy@users.noreply.github.com>
2018-12-03 20:33:50 -0500

    Add files via upload

--- mix-list3.py
+++ mix-list3.py
@@ -1,47 +1,22 @@
 #!/usr/bin/env python3
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
 
-# Original List of protocols
-proto = ['ssh', 'http', 'https']
-
-# Another list of protocols
-protoa = ['ssh', 'http', 'https']
-
-# Print the original list of protocols
-print("PRINTING the original list of protocols")
-print(proto)
+print("Printing out the entire list")
+print(list1)
 print()
 
-# Appending a string to the end of the list
-proto.append('dns') # this line will add 'dns' to the end of our list
-protoa.append('dns') # this line will add 'dns' to the end of our list
-
-# Print the changed list of protocols
-print("PRINTING the updated list of protocols. . . it now has added 'dns'")
-print(proto)
+print("Printing out one item from the list, using INDEX 1. . . ")
+print("Remember that index 1 is the 2nd item in the list")
+print(list1[1])
 print()
 
-# New list of common ports
-proto2 = [ 22, 80, 443, 53 ] # a list of common ports
-print("PRINTING the list of common ports, identified as proto2")
-print(proto2)
+# Extend the list, by adding juniper
+list1.extend(['juniper'])
+print("Print out the list, with the juniper added to the list")
+print(list1)
 print()
 
-
-#Adding the common ports list, to the original list
-proto.extend(proto2) # pass proto2 as an argument to the extend method -- then print result
-print("PRINTING the original list, with proto2 list EXTENDED")
-print(proto)
-print()
-
-#Adding the common ports list, to the protoa list
-protoa.append(proto2) # pass proto2 as an argument to the append method -- then print result
-print("PRINTING list protoa list, with proto2 list APPENDED")
-print("NOTE that the APPENDED list made a SECOND list inside of the original list")
-print(protoa)
-print()
-
-
-# Print out the changed list
-print("PRINTING the original list, with proto2 list EXTENDED")
-print("NOTE that the EXTENDED list keeps a single list")
-print(proto)
+# Append the list, by adding a list within the list
+list1.append(['10.1.0.1', '10.2.0.1', '10.3.0.1'])
+print("Print out the extended list, with the LIST added to this original list")
+print(list1)
